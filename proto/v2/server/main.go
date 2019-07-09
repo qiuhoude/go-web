c05bcb6581f150ea25e95221ec7b93ba78427802
qiuhoude <3617150@qq.com>
2019-07-09 19:08:27 +0800

    epoll tcp服务端例子

--- proto/v2/server/main.go
+++ proto/v2/server/main.go
@@ -1,34 +1,53 @@
 package main
 
 import (
-	"errors"
+	"io"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	_ "net/http/pprof"
 	"runtime"
-	"time"
 )
 import "fmt"
 
 func main() {
-	ticker := time.NewTicker(1<<63 - 1)
-	defer ticker.Stop()
-	done := make(chan bool)
-	TraceCode(errors.New("二日"))
-	time.AfterFunc(10*time.Second, func() {
-		done <- true
-	})
-	//go func() {
-	//	time.Sleep(5 * time.Second)
-	//	done <- true
-	//}()
+
+	ln, err := net.Listen("tcp", ":8972")
+	if err != nil {
+		panic(err)
+	}
+	go func() {
+		if err := http.ListenAndServe(":6060", nil); err != nil {
+			log.Fatalf("pprof failed: %v", err)
+		}
+	}()
+	var connections []net.Conn
+	defer func() {
+		for _, conn := range connections {
+			conn.Close()
+		}
+	}()
 	for {
-		select {
-		case <-done:
-			fmt.Println("Done!")
+		conn, e := ln.Accept()
+		if e != nil {
+			if ne, ok := e.(net.Error); ok && ne.Temporary() {
+				log.Printf("accept temp err: %v", ne)
+				continue
+			}
+			log.Printf("accept err: %v", e)
 			return
-		case t := <-ticker.C:
-			fmt.Println("Current time: ", t)
+		}
+		go handleConn(conn)
+		connections = append(connections, conn)
+		if len(connections)%100 == 0 {
+			log.Printf("total number of connections: %v", len(connections))
 		}
 	}
 
+}
+func handleConn(conn net.Conn) {
+	io.Copy(ioutil.Discard, conn)
 }
 
 func TraceCode(code ...interface{}) {
