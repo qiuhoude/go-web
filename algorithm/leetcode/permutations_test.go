a5dc62ed62b7cb0b14938a6b2729dc2b00b74519
qiu <361177150@qq.com>
2019-07-28 12:42:35 +0800

    排列问题

--- algorithm/leetcode/permutations_test.go
+++ algorithm/leetcode/permutations_test.go
@@ -2,6 +2,7 @@ package leetcode
 
 import (
 	"fmt"
+	"sort"
 	"testing"
 )
 
@@ -35,8 +36,49 @@ func permute(nums []int) [][]int {
 	return result
 }
 
+// 47  全排列2,nums中有重复的元素
+func permuteUnique(nums []int) [][]int {
+	if len(nums) == 0 {
+		return nil
+	}
+	if len(nums) == 1 {
+		return [][]int{{nums[0]}}
+	}
+	if len(nums) == 2 {
+		if nums[0] == nums[1] { // 与前一个数相等 只返回一种可能性
+			return [][]int{{nums[0], nums[1]}}
+		} else {
+			return [][]int{{nums[0], nums[1]}, {nums[1], nums[0]}}
+		}
+	}
+	// 先排序
+	sort.Ints(nums)
+	var result [][]int
+	for index, value := range nums {
+		var numsCopy = make([]int, len(nums))
+		copy(numsCopy, nums)
+		if index > 0 && numsCopy[index] == numsCopy[index-1] {
+			continue
+		}
+		// 将numsCopy中index这个元素给剔除掉赋值给numsSubOne
+		numsSubOne := append(numsCopy[:index], numsCopy[index+1:]...)
+		valueSlice := []int{value}
+		newSubSlice := permuteUnique(numsSubOne)
+		for _, newValue := range newSubSlice {
+			result = append(result, append(valueSlice, newValue...))
+		}
+	}
+	return result
+}
+
 func TestPermute(t *testing.T) {
 	nums := []int{1, 2, 3, 4}
 	res := permute(nums)
+	fmt.Println(res)
+}
+
+func TestPermute2(t *testing.T) {
+	nums := []int{1, 1, 2, 2}
+	res := permuteUnique(nums)
 	fmt.Println(res)
 }
