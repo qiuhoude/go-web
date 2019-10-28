879d71bf6c12cb66b2f27a0c72a115f5130c4941
qiuhoude <3617150@qq.com>
2019-10-28 11:51:57 +0800

    扩展的环形队列

--- algorithm/datastructures/queue/circular_queue.go
+++ algorithm/datastructures/queue/circular_queue.go
@@ -2,25 +2,23 @@ package queue
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
-// 环形队列
-
-// 数组队列
+// 环形数组队列
 type CircularQueue struct {
-	data     []interface{}
-	head     int
-	tail     int
-	capacity int
+	data                 []interface{}
+	head, tail, capacity int
 }
 
 func NewCircularQueue(cap int) *CircularQueue {
+	rcap := cap + 1
 	return &CircularQueue{
-		data:     make([]interface{}, cap, cap),
+		data:     make([]interface{}, rcap, rcap),
 		head:     0,
 		tail:     0,
-		capacity: cap,
+		capacity: rcap,
 	}
 }
 
@@ -30,42 +28,86 @@ func (q *CircularQueue) Dequeue() interface{} {
 	}
 	ret := q.data[q.head]
 	q.data[q.head] = nil
-	q.head++
+	q.head = (q.head + 1) % q.capacity
 	return ret
 }
 
 func (q *CircularQueue) Enqueue(v interface{}) bool {
-	len := q.Len()
-	if len >= q.capacity { //满了
+	if (q.tail+1)%q.capacity == q.head { //满了
 		return false
 	}
-	if q.tail >= q.capacity { // 进行搬迁
-		for i := 0; i < len; i++ {
-			q.data[i] = q.data[i+q.head]
-			q.data[i+q.head] = nil
-		}
-		q.head = 0
-		q.tail = len
-	}
 	q.data[q.tail] = v
-	q.tail++
+	q.tail = (q.tail + 1) % q.capacity
 	return true
 }
 
 func (q *CircularQueue) IsEmpty() bool {
-	return q.tail == q.head
+	return q.head == q.tail
 }
 
 func (q *CircularQueue) Len() int {
-	return q.tail + 1%q.capacity - q.head
+	// 注意此时getSize的逻辑:
+	// 如果tail >= head，非常简单，队列中的元素个数就是tail - head
+	// 如果tail < head，说明我们的循环队列"循环"起来了，此时，队列中的元素个数为：
+	// tail  + capacity - head;tail  + capacity 表示绕过了环
+	if q.tail >= q.head {
+		return q.tail - q.head
+	} else {
+		return q.tail + q.capacity - q.head
+	}
 }
 
 func (q *CircularQueue) String() string {
 	var sb strings.Builder
 	sb.WriteString("head ")
-	for i := q.head; i < q.tail; i++ {
-		sb.WriteString(fmt.Sprintf("%v ", q.data[i]))
+	for i := q.head; i%q.capacity != q.tail; i++ {
+		sb.WriteString(fmt.Sprintf("%v", q.data[i%q.capacity]))
+		if (i+1)%q.capacity != q.tail { // 非倒数第一个
+			sb.WriteString(",")
+		}
 	}
-	sb.WriteString("tail")
+	sb.WriteString(" tail size:" + strconv.Itoa(q.Len()))
+	sb.WriteString(", cap:" + strconv.Itoa(q.capacity))
 	return sb.String()
+}
+
+// 动态扩容和缩容
+func (q *CircularQueue) Poll() interface{} {
+	if q.IsEmpty() {
+		return nil
+	}
+	ret := q.data[q.head]
+	q.data[q.head] = nil
+	q.head = (q.head + 1) % q.capacity
+
+	len := q.Len()
+	// 长度等于容量的 1/4 进行缩容 容量的 1/2
+	if len == q.capacity/4 && q.capacity/2 != 0 {
+		q.resize(q.capacity / 2)
+	}
+	return ret
+}
+
+func (q *CircularQueue) Offer(v interface{}) bool {
+	if (q.tail+1)%q.capacity == q.head { // 满了
+		q.resize(2 * q.capacity) // 进行2倍的扩容
+	}
+	q.data[q.tail] = v
+	q.tail = (q.tail + 1) % q.capacity
+	return true
+}
+
+// 动态调整
+func (q *CircularQueue) resize(cap int) {
+	rcap := cap + 1
+	nData := make([]interface{}, rcap, rcap)
+	len := q.Len()
+	for i := 0; i < len; i++ {
+		nData[i] = q.data[(q.head+i)%q.capacity]
+	}
+	// 重新赋值
+	q.head = 0
+	q.tail = len
+	q.data = nData
+	q.capacity = rcap
 }
--- algorithm/datastructures/queue/queue_test.go
+++ algorithm/datastructures/queue/queue_test.go
@@ -42,3 +42,66 @@ func TestLinkedQueue_Dequeue(t *testing.T) {
 	}
 
 }
+
+func TestCircularQueue_Enqueue(t *testing.T) {
+	q := NewCircularQueue(5)
+	q.Enqueue(1)
+	q.Enqueue(2)
+	q.Enqueue(3)
+	q.Enqueue(4)
+	q.Enqueue(5)
+	q.Enqueue(6)
+	t.Log(q)
+}
+
+func TestCircularQueue_Dequeue(t *testing.T) {
+	q := NewCircularQueue(5)
+	q.Enqueue(1)
+	q.Enqueue(2)
+	q.Enqueue(3)
+	q.Enqueue(4)
+	q.Enqueue(5)
+	q.Enqueue(6)
+	t.Log(q)
+	t.Log(q.Dequeue())
+	t.Log(q)
+	q.Enqueue(6)
+	t.Log(q)
+	q.Dequeue()
+	q.Dequeue()
+	t.Log(q)
+	q.Enqueue(7)
+	t.Log(q)
+	q.Dequeue()
+	t.Log(q)
+	q.Dequeue()
+	t.Log(q)
+	q.Dequeue()
+	t.Log(q)
+}
+
+func TestCircularQueue_Dynamic(t *testing.T) {
+	q := NewCircularQueue(5)
+	q.Offer(1)
+	q.Offer(2)
+	q.Offer(3)
+	q.Offer(4)
+	q.Offer(5)
+	q.Offer(6)
+	t.Log(q)
+	t.Log(q.Poll())
+	t.Log(q)
+	q.Offer(6)
+	t.Log(q)
+	q.Poll()
+	q.Poll()
+	t.Log(q)
+	q.Offer(7)
+	t.Log(q)
+	q.Poll()
+	t.Log(q)
+	q.Poll()
+	t.Log(q)
+	q.Poll()
+	t.Log(q)
+}
